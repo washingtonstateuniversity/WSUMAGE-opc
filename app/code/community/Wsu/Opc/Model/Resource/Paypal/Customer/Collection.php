b40236516d4b407701460b9040614fae0c465936
Jeremy Bass <jeremybass@cableone.net>
2014-04-26 13:05:29 -0700

    puts the collection for customers

---
+++ app/code/community/Wsu/Opc/Model/Resource/Paypal/Customer/Collection.php
@@ -0,0 +1,10 @@
+<?php
+class Wsu_Opc_Model_Resource_Paypal_Customer_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract{
+	/**
+	 * Resource initialization.
+	 * @return void
+	 */
+	protected function _construct() {
+		$this->_init('wsu_opc/paypal_customer');
+	}
+}
