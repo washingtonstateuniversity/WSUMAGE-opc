20028ff8c9442992c7ebf78bd0b2ba7b099aa4ba
Jeremy Bass <jeremybass@cableone.net>
2014-04-26 13:12:54 -0700

    paypal models

---
+++ app/code/community/Wsu/Opc/Model/Paypal/Express.php
@@ -0,0 +1,17 @@
+<?php
+class Wsu_Opc_Model_Paypal_Express extends Mage_Paypal_Model_Express{
+	/**
+	 * Checkout redirect URL getter for onepage checkout (hardcode)
+	 *
+	 * @see Mage_Checkout_OnepageController::savePaymentAction()
+	 * @see Mage_Sales_Model_Quote_Payment::getCheckoutRedirectUrl()
+	 * @return string
+	 */
+	public function getCheckoutRedirectUrl() {
+		if(Mage::getStoreConfig('wsu_opc/paypal/status')){
+			return Mage::getUrl('onepage/express/start');
+		}
+
+		return parent::getCheckoutRedirectUrl();
+	}
+}
