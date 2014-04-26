05cac45155945ff2a1d86dd1ec8dc227d124683f
Jeremy Bass <jeremybass@cableone.net>
2014-04-26 12:45:14 -0700

    sets block

--- app/code/community/Wsu/Opc/Block/Lipp.php
+++ app/code/community/Wsu/Opc/Block/Lipp.php
@@ -1,19 +1,12 @@
 <?php
-class IWD_Opc_Block_Lipp extends  Mage_Core_Block_Template{
-	
+class Wsu_Opc_Block_Lipp extends  Mage_Core_Block_Template{
 	public function getJsonConfig() {
-	
 		$config = array ();
-		
 		$scheme = Mage::app()->getRequest()->getScheme();
 		$secure = false;
 		$config['baseUrl'] = Mage::getBaseUrl('link', $secure);
-		$config['paypalexpress'] = Mage::helper('opc')->getPayPalExpressUrl();
-		$config['paypalLightBoxEnabled'] = Mage::helper('opc')->getPayPalLightboxEnabled();
-		return Mage::helper ( 'core' )->jsonEncode ( $config );
+		$config['paypalexpress'] = Mage::helper('wsu_opc')->getPayPalExpressUrl();
+		$config['paypalLightBoxEnabled'] = Mage::helper('wsu_opc')->getPayPalLightboxEnabled();
+		return Mage::helper('core')->jsonEncode($config);
 	}
-	
-	
-	
-	
 }
