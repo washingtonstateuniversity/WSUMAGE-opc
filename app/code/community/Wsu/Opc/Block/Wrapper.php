4b9fe2f74902c3c533b4320a3f0ec2386c5310f7
Jeremy Bass <jeremybass@cableone.net>
2014-04-26 12:48:47 -0700

    clean up on the wrapper

--- app/code/community/Wsu/Opc/Block/Wrapper.php
+++ app/code/community/Wsu/Opc/Block/Wrapper.php
@@ -1,12 +1,11 @@
 <?php
 class Wsu_Opc_Block_Wrapper extends  Mage_Core_Block_Template {
-	const XML_PATH_DEFAULT_SHIPPING = 'wsu_opc/default/shipping';
-	const XML_PATH_GEO_COUNTRY = 'wsu_opc/geo/country';
-	const XML_PATH_GEO_CITY = 'wsu_opc/geo/city';
+	const DEFAULT_SHIPPING = 'wsu_opc/default/shipping';
+	const GEO_COUNTRY = 'wsu_opc/geo/country';
+	const GEO_CITY = 'wsu_opc/geo/city';
 
 	/**
 	 * Get one page checkout model
-	 *
 	 * @return Mage_Checkout_Model_Type_Onepage
 	 */
 	public function getOnepage() {
@@ -14,7 +13,7 @@ class Wsu_Opc_Block_Wrapper extends  Mage_Core_Block_Template {
 	}
 
 	protected function _getReviewHtml() {
-		//clear cache aftr change collection - if no magento can't find product in review block
+		//clear cache after change collection - if no magento can't find product in review block
 		Mage::app()->getCacheInstance()->cleanType('layout');
 
 		$layout = $this->getLayout();
@@ -40,8 +39,8 @@ class Wsu_Opc_Block_Wrapper extends  Mage_Core_Block_Template {
 		$config['baseUrl'] = Mage::getBaseUrl('link', true);
 		$config['isLoggedIn'] = (int) Mage::getSingleton('customer/session')->isLoggedIn();
 
-		$config['geoCountry'] =  Mage::getStoreConfig(self::XML_PATH_GEO_COUNTRY) ? Mage::helper('wsu_opc/country')->get() : false;
-		$config['geoCity'] =  Mage::getStoreConfig(self::XML_PATH_GEO_CITY) ? Mage::helper('wsu_opc/city')->get() : false;
+		$config['geoCountry'] =  Mage::getStoreConfig(self::GEO_COUNTRY) ? Mage::helper('wsu_opc/country')->get() : false;
+		$config['geoCity'] =  Mage::getStoreConfig(self::GEO_CITY) ? Mage::helper('wsu_opc/city')->get() : false;
 		$config['comment'] = Mage::helper('wsu_opc')->isShowComment();
 		$config['paypalexpress'] = Mage::helper('wsu_opc')->getPayPalExpressUrl();
 		$config['paypalLightBoxEnabled'] = Mage::helper('wsu_opc')->getPayPalLightboxEnabled();
