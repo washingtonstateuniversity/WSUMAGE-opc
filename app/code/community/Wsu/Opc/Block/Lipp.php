23271a9808572a1fe11a46da88f58777bc04df5f
Jeremy Bass <jeremybass@cableone.net>
2014-04-26 12:43:29 -0700

    sets up the link block

---
+++ app/code/community/Wsu/Opc/Block/Lipp.php
@@ -0,0 +1,19 @@
+<?php
+class IWD_Opc_Block_Lipp extends  Mage_Core_Block_Template{
+	
+	public function getJsonConfig() {
+	
+		$config = array ();
+		
+		$scheme = Mage::app()->getRequest()->getScheme();
+		$secure = false;
+		$config['baseUrl'] = Mage::getBaseUrl('link', $secure);
+		$config['paypalexpress'] = Mage::helper('opc')->getPayPalExpressUrl();
+		$config['paypalLightBoxEnabled'] = Mage::helper('opc')->getPayPalLightboxEnabled();
+		return Mage::helper ( 'core' )->jsonEncode ( $config );
+	}
+	
+	
+	
+	
+}
