2c06d4fd206cf36ad2eac569a1ff19fb710e5557
jeremyBass <jeremybass@cableone.net>
2014-04-29 09:20:39 -0700

    clearly marks that T&C is truned off

--- app/code/community/Wsu/Opc/Model/System/Config/Source/Terms/Comment.php
+++ app/code/community/Wsu/Opc/Model/System/Config/Source/Terms/Comment.php
@@ -2,6 +2,6 @@
 class Wsu_Opc_Model_System_Config_Source_Terms_Comment{
     public function getCommentText(){
 		$agreeable = Mage::getStoreConfigFlag('checkout/options/enable_agreements');
-        return $agreeable?false:"Untill you trun on 'agreements' this will affect nothing.  To change this set to yes under checkout settings.  <a href='".Mage::helper("adminhtml")->getUrl("adminhtml/system_config/edit/section/checkout")."' >Click here to change</a>";
+        return $agreeable?false:"<b>Terms and Conditions is truned off.</b>  Untill you trun on 'agreements' this will affect nothing.  To change this set to yes under checkout settings.  <a href='".Mage::helper("adminhtml")->getUrl("adminhtml/system_config/edit/section/checkout")."' >Click here to change</a>";
     }
 }
