671a7be994a9cd98b100b8b4291722e487b3ed0f
Jeremy Bass <jeremybass@cableone.net>
2014-08-04 23:43:45 -0700

    lets dbl up to ensure it's removed, disable that is

--- js/wsu/opc/checkout.js
+++ js/wsu/opc/checkout.js
@@ -99,6 +99,7 @@ WSU.OPC = {
 			jQuery('#co-payment-form').find('dd ul').hide();
 			jQuery(this).closest('dt').next('dd').find('ul').show(function(){
 				jQuery(this).find('input:disabled').attr("disabled",false);	
+				jQuery(this).find('input').removeAttr("disabled");	
 			});
 			WSU.OPC.validatePayment();
 		});
