43f7bc425b861e6c67a4284ffdcec3f6a17cd3d2
Jeremy Bass <jeremybass@cableone.net>
2016-10-31 09:06:36 -0700

    reset match

--- skin/frontend/base/default/js/wsu/opc/wsu.opc.js
+++ skin/frontend/base/default/js/wsu/opc/wsu.opc.js
@@ -3,10 +3,14 @@ jQuery.WSU=jQuery.WSU||{};
     
     Validation.add('validate-pobox','We cannot ship to your P.O. Please check the button to ship to a different address.',function(field_value) {
         // setup a regex var for pretty much every possibility of PO box...
-        var regex = /[p]*(ost)*\.*\s*[o|0]*(ffice)*\.*\s*[b][o|0][x]/gi;
+        var regex = /[P|p]*(OST|ost)*\.*\s*[O|o|0]*(ffice|FFICE)*\.*\s*[B|b][O|o|0][X|x]/gi;
         // if the field_value contains PO Box
     
         if(field_value.match(regex)) {
+            /*if (document.getElementById('billing:use_for_shipping_yes').checked === true) {
+                return false;
+                //return false;
+            }*/
             return false;
         } else if(!field_value.match(regex)) {
             return true;
