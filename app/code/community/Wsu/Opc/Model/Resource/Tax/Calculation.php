2a10f5b5745436db1a868cf881c5f24f40454caf
Jeremy Bass <jeremybass@cableone.net>
2016-11-03 12:04:31 -0700

    note to float on

--- app/code/community/Wsu/Opc/Model/Resource/Tax/Calculation.php
+++ app/code/community/Wsu/Opc/Model/Resource/Tax/Calculation.php
@@ -1,6 +1,7 @@
 <?php
 class Wsu_Opc_Model_Resource_Tax_Calculation extends Mage_Tax_Model_Resource_Calculation {
-    protected function _getRates($request) {
+    protected function _getRates($request) {  // this should be optional, and should account for Country as well
+        //also, why not just fix the wild card ( long run thinking )
         $postcode = substr($request->getPostcode(),0,5);
         $request->setPostcode($postcode);
         return parent::_getRates($request);
