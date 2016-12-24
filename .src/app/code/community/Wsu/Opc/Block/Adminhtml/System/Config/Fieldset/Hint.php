944f312308b19c5af96e79913a742bb54f654b87
Jeremy Bass <jeremybass@cableone.net>
2016-12-24 15:45:23 -0800

    feild hint correction on MAGE expected

--- .src/app/code/community/Wsu/Opc/Block/Adminhtml/System/Config/Fieldset/Hint.php
+++ .src/app/code/community/Wsu/Opc/Block/Adminhtml/System/Config/Fieldset/Hint.php
@@ -2,7 +2,7 @@
 class Wsu_Opc_Block_Adminhtml_System_Config_Fieldset_Hint extends Mage_Adminhtml_Block_Abstract implements Varien_Data_Form_Element_Renderer_Interface
 {
     protected $_template = 'wsu/opc/system/config/fieldset/hint.phtml';
-    public function render()//Varien_Data_Form_Element_Abstract $element)
+    public function render(Varien_Data_Form_Element_Abstract $element)// @codingStandardsIgnoreLine
     {
         return $this->toHtml();
     }
