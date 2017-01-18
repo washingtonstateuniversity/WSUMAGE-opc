dcb68ac39e2ab026c1e61c96717f9fbc449d1d38
Jeremy Bass <jeremybass@cableone.net>
2017-01-18 08:04:03 -0800

    clear issue of a decluration warrning on active

--- .src/app/code/community/Wsu/Opc/Block/System/Config/Form/Fieldset/Extensions.php
+++ .src/app/code/community/Wsu/Opc/Block/System/Config/Form/Fieldset/Extensions.php
@@ -1,7 +1,7 @@
 <?php
 class Wsu_Opc_Block_System_Config_Form_Fieldset_Extensions extends Mage_Adminhtml_Block_System_Config_Form_Field
 {
-    protected function _getElementHtml()//Varien_Data_Form_Element_Abstract $element)
+    protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element) // @codingStandardsIgnoreLine
     {
         $version = Mage::getConfig()->getModuleConfig("Wsu_Opc")->version;
         return '<span class="notice">' . $version . '</span>';
